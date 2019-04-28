6804dbc9deb8d3b44e2f5d7c07112d085382679b
大鹏 <davidmaster@163.com>
2019-04-28 19:01:53 +0800

    重构相关逻辑

---
+++ ApiDemo/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace ApiDemo.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Index()
+        {
+            return Json("asdf");
+        }
+    }
+}
