e8a71d1e497b9f2f51a9a8b1abff84f0077a5114
大鹏 <davidmaster@163.com>
2019-04-29 18:41:59 +0800

    优化日志的记录

--- Demo/WebDemo/Global.asax.cs
+++ Demo/WebDemo/Global.asax.cs
@@ -20,17 +20,39 @@ namespace WebDemo
         public override void Init()
         {
             BeginRequest += Global_BeginRequest;
-            PreSendRequestContent += (s, e) => Trace.Flush();
+            PreSendRequestContent += Global_PreSendRequestContent;
             base.Init();
+        }
+        private Stopwatch _stopwatch;
+        private void Global_PreSendRequestContent(object sender, EventArgs e)
+        {
+            _stopwatch.Stop();
+            double timing = _stopwatch.Elapsed.TotalMilliseconds;
+            if (timing > 500)
+            {
+                Trace.TraceWarning("API用时过长");
+            }
+            Trace.WriteLine(timing + " ms", "WebApi Timing");
+            Trace.WriteLine("Request End", "WebApi Log");
+            Trace.Flush();
         }
 
         private void Global_BeginRequest(object sender, EventArgs e)
         {
-            var app = (HttpApplication) sender;
-            Trace.WriteLine(app.Context.Request.Url.ToString(),"*url*");
-            Trace.WriteLine(Dns.GetHostName(),"*HostName*");
-            Trace.WriteLine(string.Join(",", Dns.GetHostAddresses(Dns.GetHostName()).Select(it => it.ToString()).Where(it => it.Contains("."))), "HostAddresses");
-            Trace.WriteLine("127.0.0.1", "RealIP");
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+            var app = (HttpApplication)sender;
+
+            Trace.WriteLine(Dns.GetHostName(), "*HostName*");
+            var hostAddress = string.Join(",",
+                Dns.GetHostAddresses(Dns.GetHostName()).Select(it => it.ToString()).Where(it => it.Contains(".")));
+            Trace.WriteLine(hostAddress, "HostAddresses");
+            Trace.WriteLine(hostAddress, "RealIP");
+            Trace.WriteLine(app.Context.Request.Url.ToString(), "*Url*");
+            //var contentType =app.Context.Request.RequestContext.HttpContext..ContentType.Content?.Headers?.ContentType?.MediaType ?? "";
+            Trace.WriteLine(app.Context.Request.ContentType, "ContentType");
+            Trace.WriteLine(app.Context.Request.UrlReferrer?.AbsoluteUri, "UrlReferrer");
+            Trace.WriteLine(app.Context.Request.UserAgent, "UserAgent");
            
 
         }
