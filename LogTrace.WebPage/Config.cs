f81971573cda2bdc3cafad179f0f601a85d0346b
大鹏 <davidmaster@163.com>
2019-05-24 11:02:16 +0800

    移除对Global文件的依赖

---
+++ LogTrace.WebPage/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using LogTrace.WebPage;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+[assembly:System.Web.PreApplicationStartMethod(typeof(LogTraceConfig),"Init")]
+namespace LogTrace.WebPage
+{
+    public sealed class LogTraceConfig
+    {
+        [ThreadStatic]
+        private static bool _init;
+        public static void Init()
+        {
+            if (_init) return;
+            _init = true;
+            DynamicModuleUtility.RegisterModule(typeof(LogTraceModule));
+        }
+    }
+}
